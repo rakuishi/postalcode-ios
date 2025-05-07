012e11d1913a09937fae5e2487c75bcbb4f09a04
rakuishi <rakuishi@gmail.com>
2025-05-07 19:03:43 +0900

    Swift: PostalCodeTests

---
+++ PostalCodeTests/PostalCodeTests.swift
@@ -0,0 +1,23 @@
+//
+//  PostalCodeTests.swift
+//  PostalCodeTests
+//
+//  Created by Koichiro OCHIISHI on 2025/05/07.
+//  Copyright © 2025 OCHIISHI Koichiro. All rights reserved.
+//
+
+import XCTest
+@testable import PostalCode
+
+final class PostalCodeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+}
